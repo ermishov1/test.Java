2432d8d9aa3255dce2c431a712108ac982291bc3
User <ermishov03@gmail.com>
2021-10-16 20:00:01 +0800

    vd

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float num_1 = 8, num_2 = 6, num_3 = 4, num_4 = 2, num_5 = 3, res;
+    res = num_1 + num_2 - num_3 / num_4 * num_5;
+    System.out.print (res);
+    }
+}
